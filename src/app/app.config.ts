4a484a03bbf01549e8537a4d20d5a9929c385e3d
Rebeca Aguiar <rebeca.coutinho@solutis.com.br>
2025-11-14 16:44:16 -0300

    feat: integrate PrimeNG components and themes, update styles and routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,26 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import { ApplicationThemePreset } from './core/styles/primeng.theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: ApplicationThemePreset,
+        options: {
+          prefix: 'p',
+          darkModeSelector: false,
+          cssLayer: {
+            name: 'primeng',
+            order: 'primeng'
+          }
+        }
+      }
+    })
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.routes').then((m) => m.authRoutes),
+  },
+  {
+    path: "",
+    canActivate: [],
+    loadChildren: () => import("./modules/dashboard/dashboard.routes").then((m) => m.dashboardRoutes),
+  }
+];
